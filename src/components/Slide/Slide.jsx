03655fd9c12725a74abc5eb404c4753134497fa2
syedalamin <syedalamin2580@gmail.com>
2023-06-08 00:08:25 +0600

    home slide done

--- src/components/Slide/Slide.jsx
+++ src/components/Slide/Slide.jsx
@@ -1,16 +1,20 @@
 
 
-const Slide = ({photo, subHeading, Heading,  title}) => {
+const Slide = ({ photo, subHeading, Heading, title,  }) => {
     return (
-        <div>
-            <div className="grid grid-cols-2">
-                <div>
-                    <h5 className="">{subHeading}</h5>
-                    <h2 className="">{Heading}</h2>
-                    <p>{title}</p>
-                </div>
-                <div>
-                    <img src={photo} />
+        <div className="h-fit flex items-center">
+            <div>
+                <div className="grid md:grid-cols-2 py-10 gap-10">
+                    <div className="md:flex items-center">
+                        <div>
+                            <h5 className=" font-semibold text-lg text-orange-600">{subHeading}</h5>
+                            <h2 className="text-3xl text-black font-bold py-3">{Heading}</h2>
+                            <p className="text-black font-semibold">{title}</p>
+                        </div>
+                    </div>
+                    <div className=" flex justify-center items-baseline ">
+                        <img src={photo} />
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,5 +1,5 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Pagination, Navigation } from "swiper";
+import { Autoplay, Pagination, } from "swiper";
 import Slide from "../../../components/Slide/Slide";
 import "swiper/css";
 import "swiper/css/pagination";
@@ -11,21 +11,23 @@ import slider_2 from '../../../assets/SwiperSlide/slider-banner-2.png'
 
 const Home = () => {
     return (
-        <div className="bg-[#fdf6ed]">
-            <div className="w-10/12 mx-auto">
+        <div className="bg-[#edfdfc]">
+            <div className="w-10/12 mx-auto ">
                 <Swiper
-                    // spaceBetween={50}
-                    // centeredSlides={true}
-                    // autoplay={{
-                    //     // delay: 5000,
-                    //     // disableOnInteraction: false,
-                    // }}
-                    pagination={{
-                        clickable: true,
+                    spaceBetween={50}
+                    centeredSlides={true}
+                    autoplay={{
+                        delay: 5000,
+                        disableOnInteraction: false,
                     }}
-                    navigation={true}
-                    modules={[Autoplay, Pagination, Navigation]}
-                    className="mySwiper h-screen "
+
+                    pagination={{
+                        dynamicBullets: true,
+                    }}
+              
+  
+                    modules={[Autoplay, Pagination, ]}
+                    className="mySwiper "
                 >
                     <SwiperSlide> <Slide photo={slider_2} subHeading=" A Journey to Discover New Horizons" Heading="Exploring the Wonders of Language Learning" title="Welcome to the fascinating world of language learning! Whether you're embarking on this journey for personal enrichment, academic purposes, or to communicate with people from diverse cultures, learning a new language opens up a multitude of opportunities. In this text, we'll delve into the benefits, challenges, and effective strategies for mastering a foreign tongue." ></Slide>  </SwiperSlide>
                     <SwiperSlide> <Slide photo={slider_1} subHeading=" A Journey to Discover New Horizons " Heading="Why Learn a New Language?" title='Learning a new language has numerous advantages that extend beyond the ability to communicate. It enhances cognitive abilities, boosts memory, and improves problem-solving skills. Moreover, it broadens your perspective by immersing you in a different culture and fostering empathy and understanding. Additionally, it opens doors to professional opportunities, increases employability, and facilitates international travel.'></Slide>  </SwiperSlide>
