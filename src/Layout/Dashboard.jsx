1c2e57a4b525d7d8bc12200ab0efd65ae5f36e2e
syedalamin <syedalamin2580@gmail.com>
2023-06-09 07:46:50 +0600

    my cart add

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import { FaBookReader, FaHome, FaShoppingCart, FaUsers, FaWallet, } from 'react-icons/fa'
 const Dashboard = () => {
     return (
@@ -11,15 +11,15 @@ const Dashboard = () => {
             </div>
             <div className="drawer-side">
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
+                <ul className="menu p-4 w-60 h-full bg-[#cef7f4] text-base-content">
                     {/* Sidebar content here */}
-                    <li><Link><FaHome></FaHome>User Home</Link></li>
-                    <li><Link to='/dashboard/mycart'><FaShoppingCart></FaShoppingCart> Enrolled</Link></li>
-                    <li><Link><FaWallet></FaWallet>Payment History</Link></li>
+                    <li><NavLink to='/dashboard/home'><FaHome></FaHome>User Home</NavLink></li>
+                    <li><NavLink to='/dashboard/mycart'><FaShoppingCart></FaShoppingCart> Enrolled</NavLink></li>
+                    <li><NavLink to='/dashboard/history'><FaWallet></FaWallet>Payment History</NavLink></li>
                     <div className="divider"></div>
-                    <li><Link to='/'><FaHome></FaHome>Home</Link></li>
-                    <li><Link to='/instructors'><FaUsers></FaUsers>Instructors</Link></li>
-                    <li><Link to='/classes'><FaBookReader></FaBookReader>Classes</Link></li>
+                    <li><NavLink to='/'><FaHome></FaHome>Home</NavLink></li>
+                    <li><NavLink to='/instructors'><FaUsers></FaUsers>Instructors</NavLink></li>
+                    <li><NavLink to='/classes'><FaBookReader></FaBookReader>Classes</NavLink></li>
                 </ul>
 
             </div>
--- src/pages/Dashboard/MyCart/MyCart.jsx
+++ src/pages/Dashboard/MyCart/MyCart.jsx
@@ -1,13 +1,20 @@
 import { Helmet } from "react-helmet-async";
+import useCart from "../../../hooks/useCart";
 
 
 const MyCart = () => {
+    const [cart] = useCart();
+    const total = cart.reduce((sum, item)=> item.price + sum, 0)
     return (
         <div>
             <Helmet>
                 <title>Dashboard | My Cart</title>
             </Helmet>
-            <h2>my cart</h2>
+            <div className="uppercase">
+                <h3 className="text-3xl">Total Class: {cart.length}</h3>
+                <h3 className="text-3xl">Total Price: ${total}</h3>
+                <button className="bg-teal-600 hover:bg-teal-800 btn btn-sm text-white ">Pay</button>
+            </div>
         </div>
     );
 };
